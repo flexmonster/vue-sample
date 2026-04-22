30c9ae9ee1d5c3f680755a195a2f9404c06a315e
marviem <45265050+marviem@users.noreply.github.com>
2026-04-22 16:10:21 +0300

    updating the sample project

---
+++ src/views/PivotTableDemo.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { FMFlexmonster } from "@flexmonster/vue";
+
+const state = {
+  id: "fm-state",
+  dataset: {
+    dataSource: {
+      // Step 4.a. Specify the URL of your Flexmonster server
+      url: "wss://demo-server.flexmonster.com:9500",
+      // Uncomment the line below to connect to your local Flexmonster server
+      // url: "ws://localhost:9501",
+      // Step 4.b. Specify the name of your data source
+      name: "StateNames",
+    },
+  },
+  slice: {
+    rows: [
+      {
+        name: "Year",
+      },
+      {
+        name: "Gender",
+      },
+      {
+        name: "Name",
+      },
+    ],
+    values: [
+      {
+        name: "Count",
+        aggregation: "sum",
+      },
+    ],
+    columns: [
+      {
+        name: "State",
+      },
+    ],
+  },
+};
+</script>
+
+<template>
+  <div className="text-wrap">
+    <h1 className="page-title">Flexmonster 3.0 (preview)</h1>
+    <p>
+      Welcome to the preview of our next major release. The pivot table below is connected to our demo server and
+      displays the 150MB dataset with 5.6M records of US kids' naming trends&nbsp;(1910-2014).
+    </p>
+    <p>Check out how to add Flexmonster 3.0 to your project:</p>
+  </div>
+  <ol className="steps-list">
+    <li data-index="01">
+      <a
+        href="https://prerelease.flexmonster.com/doc/vue/install-flexmonster-server/?r=rm_vue_30"
+        target="blank"
+        >Install the Flexmonster server</a
+      >
+    </li>
+    <li data-index="02">
+      <a
+        href="https://prerelease.flexmonster.com/doc/vue/embed-flexmonster-ui/?r=rm_vue_30"
+        target="blank"
+        >Embed the Flexmonster UI</a
+      >
+    </li>
+    <li data-index="03">
+      <a
+        href="https://prerelease.flexmonster.com/doc/vue/supported-data-sources/?r=rm_vue_30"
+        target="blank"
+        >Feed your data</a
+      >
+    </li>
+  </ol>
+  <div className="pivot-container">
+    <FMFlexmonster :state="state" />
+  </div>
+</template>
